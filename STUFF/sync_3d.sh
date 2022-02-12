dc9a8ceb780a8fadfed4d967fb3bb8f1608bfa81
Pacman Graphics <66055060+HakkaTjakka@users.noreply.github.com>
2022-02-12 02:26:55 +0100

    Add files via upload

---
+++ STUFF/sync_3d.sh
@@ -0,0 +1,5 @@
+#rsync -avu --delete "/media/pacman/PACMAN/SAVES/HOLLAND/region3d/" "/opt/holland/region3d/"
+#rsync -av --delete /media/pacman/PACMAN/SAVES/HOLLAND/region3d/ /opt/holland/region3d/
+
+rsync -av /media/pacman/PACMAN/SAVES/HOLLAND/region3d/ /opt/holland/region3d/
+
